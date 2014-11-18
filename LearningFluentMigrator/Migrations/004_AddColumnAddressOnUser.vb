25a63dbbbe9a3febcf70d3f69b549514181a1850
HOMMA Takashi <hommatks@gmail.com>
2014-11-18 17:25:10 +0900

    UserテーブルにAddress列を追加する

---
+++ LearningFluentMigrator/Migrations/004_AddColumnAddressOnUser.vb
@@ -0,0 +1,14 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(4)> Public Class AddColumnAddressOnUser : Inherits Migration
+
+        Public Overrides Sub Up()
+            Alter.Table("User").AddColumn("Address").AsString(255).Nullable()
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("Address").FromTable("User")
+        End Sub
+    End Class
+End Namespace
