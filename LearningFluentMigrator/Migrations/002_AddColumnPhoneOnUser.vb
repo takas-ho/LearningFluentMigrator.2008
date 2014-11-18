285fec67e9cf8dc463247adb26dcb354501a9504
HOMMA Takashi <hommatks@gmail.com>
2014-11-18 17:20:26 +0900

    UserテーブルにPhone列を追加する

---
+++ LearningFluentMigrator/Migrations/002_AddColumnPhoneOnUser.vb
@@ -0,0 +1,14 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(2)> Public Class AddColumnPhoneOnUser : Inherits Migration
+
+        Public Overrides Sub Up()
+            Alter.Table("User").AddColumn("Phone").AsAnsiString(13).Nullable()
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("Phone").FromTable("User")
+        End Sub
+    End Class
+End Namespace
